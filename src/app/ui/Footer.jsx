e835aaa940b1d8d79b443261833c77deb731a2e1
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-02-08 21:12:09 +0400

    refactoring rwd in footer

--- src/app/ui/Footer.jsx
+++ src/app/ui/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className="w-full border-t-[2px] border-gray-300 bg-stone-100 py-10">
-            <div className="du_container lg:px-2md:flex-row flex flex-col items-center justify-between gap-2 px-2 md:px-4">
+            <div className="du_container flex flex-col items-center justify-between gap-2 px-2 md:flex-row md:px-4 lg:px-2">
                 <div className="flex gap-2">
                     <a
                         href="#"
